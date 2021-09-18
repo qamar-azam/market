1dacccf20a50ee88d5ea902c3eb1d48a64c7f48d
Qamar <qazam@lucidya.com>
2021-09-18 19:08:53 +0500

    add state persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,35 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  combineReducers,
+} from "@reduxjs/toolkit"
+import storage from "redux-persist/lib/storage"
+import { persistStore, persistReducer } from "redux-persist"
 import productReducer from "../components/product/productSlice"
 import basketReducer from "../components/basket/basketSlice"
 
-export const store = configureStore({
-  reducer: {
-    product: productReducer,
-    basket: basketReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+const rootReducer = combineReducers({
+  product: productReducer,
+  basket: basketReducer,
 })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+})
+
+export const persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
